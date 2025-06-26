e1f9b1ef3ebac5f1ace2750a37dd0be816409377
Praveen Kamisetty <pkamisetty@hms.com>
2025-06-26 13:04:04 -0400

    Reapply "Commit changes to IMR"
    This reverts commit 8369169b02b6735e03eeee12339230cc28392860.

---
+++ HelloWorld_iOrion.cs
@@ -0,0 +1,7 @@
+//This is hello world from iOrion application
+
+// I am making changes to both iOrion and IMR, but I want to checkin changes only to IMR application.
+
+//If by mistake I check in both the changes to the repository, I need to figure out how to revert and check in only one of them.
+
+//In this scenario, I will push to Orion / commit the changes to both the hello world files and try to revert it back and then commit only one of them.
