5c953749945ab639b2acc07fb857530efc1cb0cd
Praveen Kamisetty <pkamisetty@hms.com>
2025-06-26 16:20:06 -0400

    Fixed the commit from earlier.
    I have to revert the changes, that will remove the files from the iOrion and IMR folder or in this case both IMR and iOrion files in the githubTest folder, prior to revert, I copied both the files in to another backup folder, after revert, I copied only the IMR file into the githubTest folder and now I cam checking in, this will ensure I have only the changes to IMR committed.

---
+++ HelloWorld_IMR.cs
@@ -0,0 +1,2 @@
+//This is a hello world from IMR application.
+//Okay I have the reverted the changes and making change only to this file, now I will commit this file and the other file is not pushed.
